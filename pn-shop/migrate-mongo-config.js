5c2bb089f4bfb8cc4cd08b8bab7b7c13e67ca342
Pavelklm <pahanklm01@gmail.com>
2024-04-30 10:42:15 +0300

    fix bags

--- pn-shop/migrate-mongo-config.js
+++ pn-shop/migrate-mongo-config.js
@@ -9,7 +9,7 @@ const config = {
     url: process.env.NEXT_PUBLIC_DB_URL,
 
     // TODO Change this to your database name:
-    databaseName: process.env.NEXT_PUBLIC_DB_NAME || 'pn-shop',
+    databaseName: process.env.NEXT_PUBLIC_DB_NAME || 'vercel',
 
     options: {
       useNewUrlParser: true, // removes a deprecation warning when connecting
