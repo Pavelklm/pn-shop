8ba81d6d5dee6eabac237a691512b16db8c35aff
Pavelklm <pahanklm01@gmail.com>
2024-10-23 17:21:05 +0300

    add footer and subscribe

---
+++ pn-shop/components/elements/SubscribeSocial_media/SubscribeSocial_media-facebook.tsx
@@ -0,0 +1,54 @@
+/* eslint-disable max-len */
+type SubscribeSocialMediaFacebook = {
+  className: string
+}
+
+// Define the component function
+const SubscribeSocialMediaFacebook: React.FC<SubscribeSocialMediaFacebook> = ({
+  className,
+}) => (
+  <>
+    <svg
+      width='40.000000'
+      height='40.000000'
+      viewBox='0 0 40 40'
+      xmlns='http://www.w3.org/2000/svg'
+      xmlnsXlink='http://www.w3.org/1999/xlink'
+      fill='none'
+    >
+      <defs>
+        <clipPath id='clip275_14974'>
+          <rect
+            id='facebook 1'
+            width='18.000000'
+            height='18.000000'
+            transform='translate(11.000000 11.000000)'
+            fill='white'
+            fill-opacity='0'
+          />
+        </clipPath>
+      </defs>
+      <circle id='Ellipse 3' cx='20.000000' cy='20.000000' r='20.000000' />
+      <rect
+        id='facebook 1'
+        width='18.000000'
+        height='18.000000'
+        transform='translate(11.000000 11.000000)'
+        fill='#FFFFFF'
+        fill-opacity='0'
+      />
+      <g clip-path='url(#clip275_14974)'>
+        <path
+          className={className}
+          id='Vector'
+          d='M22.99 13.98L24.64 13.98L24.64 11.12C24.35 11.08 23.38 11 22.24 11C19.87 11 18.25 12.49 18.25 15.22L18.25 17.75L15.64 17.75L15.64 20.94L18.25 20.94L18.25 29L21.46 29L21.46 20.95L23.96 20.95L24.36 17.75L21.45 17.75L21.45 15.54C21.46 14.62 21.7 13.98 22.99 13.98Z'
+          fill='#B3B3B3'
+          fill-opacity='1.000000'
+          fill-rule='nonzero'
+        />
+      </g>
+    </svg>
+  </>
+)
+
+export default SubscribeSocialMediaFacebook
