a42accade063e2dd972b17ea02dba81532ff74ad
Pavelklm <pahanklm01@gmail.com>
2024-12-24 11:51:13 +0200

    add validation to subscribe email "client"

--- pn-shop/components/modules/Subscribe/Subscribe.tsx
+++ pn-shop/components/modules/Subscribe/Subscribe.tsx
@@ -9,31 +9,34 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
 import toast from 'react-hot-toast'
+import validator from 'validator'
 
 const Subscribe = () => {
   const { lang, translations } = useLang()
   const isMedia1200 = useMediaQuery(1200)
   const dispatch = useAppDispatch()
   const [email, setEmail] = useState('')
+  const [error, setError] = useState('')
   const isSubscribed = useAppSelector((state) => state.subscribe.subscribed)
   const isLoadingSubscribe = useAppSelector((state) => state.subscribe.loading)
   const user = useAppSelector((state) => state.user.user)
+  const [isClicked, setIsClicked] = useState(false)
+
   useEffect(() => {
     if (user.subscribed) {
       dispatch(setSubscribed())
     }
-  }, [dispatch, isSubscribed, user])
+  }, [dispatch, user.subscribed])
 
   const handleInputFocus = (
-    e: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement, Element>
+    e: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
     if (!e.target.value) {
       e.target.classList.remove('placeholder')
     }
   }
-
   const handleInputBlur = (
-    e: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement, Element>
+    e: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
     if (e.target.value) {
       e.target.classList.remove('placeholder')
@@ -43,19 +46,85 @@ const Subscribe = () => {
     }
   }
 
+  const validateEmail = () => {
+    if (!email) {
+      const errorMessage =
+        translations[lang].errors.email_required || 'Email is required'
+      setError(errorMessage)
+      toast.error(errorMessage)
+      return false
+    }
+    if (!validator.isEmail(email)) {
+      const errorMessage =
+        translations[lang].errors.invalid_email || 'Invalid email'
+      setError(errorMessage)
+      toast.error(errorMessage)
+      return false
+    }
+    setError('')
+    return true
+  }
+
   const handleSubscribe = (
     event: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ) => {
     event.preventDefault()
+    setIsClicked(true)
+    console.log(isClicked)
+    if (!validateEmail()) return
+    triggerSubscribe(dispatch, email)
+  }
 
-    if (!email) {
-      toast.error(translations[lang].errors.email_required)
-    }
-
-    if (email) {
-      triggerSubscribe(dispatch, email)
+  const emailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value)
+    console.log(email)
+    if (isClicked) {
+      setIsClicked(false)
     }
   }
+
+  const renderForm = () => (
+    <div className='subscribe__media_wrapper'>
+      <form className='subscribe__form'>
+        <label className='subscribe__label'>
+          <input
+            className={`subscribe__input placeholder ${!email ? '' : isClicked && error ? 'input-error' : ''}`}
+            type='email'
+            onFocus={handleInputFocus}
+            onBlur={handleInputBlur}
+            value={email}
+            onChange={emailChange}
+            placeholder={translations[lang].subscribe.placeholder}
+          />
+          <button
+            onClick={handleSubscribe}
+            className='btn-reset subscribe__btn'
+          >
+            {isLoadingSubscribe ? (
+              <FontAwesomeIcon icon={faSpinner} spin />
+            ) : (
+              translations[lang].subscribe.button
+            )}
+          </button>
+        </label>
+      </form>
+      <SubscribeSocialLinks className='subscribe__social_media' />
+    </div>
+  )
+
+  const renderSuccessMessage = () => (
+    <div className='subscribe__media_wrapper__success'>
+      <div className='subscribe__success'>
+        <div className='subscribe__success__text1'>
+          {translations[lang].subscribe.success1}
+        </div>
+        <div className='subscribe__success__text2'>
+          {translations[lang].subscribe.success2}
+        </div>
+      </div>
+      <SubscribeSocialLinks className='subscribe__social_media__success' />
+    </div>
+  )
 
   return !isMedia1200 ? (
     <div className='container'>
@@ -80,53 +149,7 @@ const Subscribe = () => {
               </p>
             </div>
           </div>
-          {!isSubscribed ? (
-            <>
-              <div className='subscribe__media_wrapper'>
-                <form className='subscribe__form'>
-                  <label className='subscribe__label'>
-                    <input
-                      className='subscribe__input placeholder'
-                      type='email'
-                      onFocus={handleInputFocus}
-                      onBlur={handleInputBlur}
-                      value={email}
-                      onChange={(event) => setEmail(event.target.value)}
-                    />
-
-                    <span className='subscribe__buttons__search__floating_label'>
-                      {translations[lang].subscribe.placeholder}
-                    </span>
-                    <button
-                      onClick={handleSubscribe}
-                      className='btn-reset subscribe__btn'
-                    >
-                      {isLoadingSubscribe ? (
-                        <FontAwesomeIcon icon={faSpinner} spin />
-                      ) : (
-                        translations[lang].subscribe.button
-                      )}
-                    </button>
-                  </label>
-                </form>
-                <SubscribeSocialLinks className='subscribe__social_media' />
-              </div>
-            </>
-          ) : (
-            <>
-              <div className='subscribe__media_wrapper__success'>
-                <div className='subscribe__success'>
-                  <div className='subscribe__success__text1'>
-                    {translations[lang].subscribe.success1}
-                  </div>
-                  <div className='subscribe__success__text2'>
-                    {translations[lang].subscribe.success2}
-                  </div>
-                </div>
-                <SubscribeSocialLinks className='subscribe__social_media__success' />
-              </div>
-            </>
-          )}
+          {!isSubscribed ? renderForm() : renderSuccessMessage()}
         </div>
       </div>
     </div>
@@ -147,59 +170,12 @@ const Subscribe = () => {
             <h3 className='subscribe__title'>
               {translations[lang].subscribe.title}
             </h3>
-
             <p className='subscribe__description'>
               {translations[lang].subscribe.description}
             </p>
           </div>
         </div>
-        {!isSubscribed ? (
-          <>
-            <div className='subscribe__media_wrapper'>
-              <form className='subscribe__form'>
-                <label className='subscribe__label'>
-                  <input
-                    className='subscribe__input placeholder'
-                    type='email'
-                    onFocus={handleInputFocus}
-                    onBlur={handleInputBlur}
-                    value={email}
-                    onChange={(event) => setEmail(event.target.value)}
-                  />
-
-                  <span className='subscribe__buttons__search__floating_label'>
-                    {translations[lang].subscribe.placeholder}
-                  </span>
-                  <button
-                    onClick={handleSubscribe}
-                    className='btn-reset subscribe__btn'
-                  >
-                    {isLoadingSubscribe ? (
-                      <FontAwesomeIcon icon={faSpinner} spin />
-                    ) : (
-                      translations[lang].subscribe.button
-                    )}
-                  </button>
-                </label>
-              </form>
-              <SubscribeSocialLinks className='subscribe__social_media' />
-            </div>
-          </>
-        ) : (
-          <>
-            <div className='subscribe__media_wrapper__success'>
-              <div className='subscribe__success'>
-                <div className='subscribe__success__text1'>
-                  {translations[lang].subscribe.success1}
-                </div>
-                <div className='subscribe__success__text2'>
-                  {translations[lang].subscribe.success2}
-                </div>
-              </div>
-              <SubscribeSocialLinks className='subscribe__social_media__success' />
-            </div>
-          </>
-        )}
+        {!isSubscribed ? renderForm() : renderSuccessMessage()}
       </div>
     </div>
   )
