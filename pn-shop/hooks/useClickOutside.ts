bb0c4a4c20f249eb4056e64ca717745f415fbc12
pavel <pahanklm01@gmail.com>
2025-04-15 09:28:23 +0300

    fix bags with build

--- pn-shop/hooks/useClickOutside.ts
+++ pn-shop/hooks/useClickOutside.ts
@@ -1,22 +1,21 @@
-import { MutableRefObject, useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 export const useClickOutside = () => {
-  const ref = useRef() as MutableRefObject<HTMLDivElement>
+  const ref = useRef<HTMLDivElement>(null)
   const [open, setOpen] = useState(false)
 
   const toggle = () => setOpen(!open)
 
   useEffect(() => {
     const handleClickOutside = (e: MouseEvent) => {
-      if (!ref.current.contains(e.target as HTMLDivElement)) {
+      if (ref.current && !ref.current.contains(e.target as Node)) {
         setOpen(false)
       }
     }
 
     document.addEventListener('mousedown', handleClickOutside)
-
     return () => document.removeEventListener('mousedown', handleClickOutside)
-  }, [ref])
+  }, [])
 
   return { open, setOpen, toggle, ref }
 }
