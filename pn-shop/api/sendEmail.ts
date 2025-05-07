8e6bab6aa2bbbc1ae18ff66db3d5f132ddbc0f41
pavel <pahanklm01@gmail.com>
2025-05-07 16:15:47 +0300

    fix vercel

--- pn-shop/api/sendEmail.ts
+++ pn-shop/api/sendEmail.ts
@@ -1,6 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
-import api from './apiInstance'
+import api from '../api/apiInstance'
 
 export const sendEmailFx = createAsyncThunk(
   'email/send',
